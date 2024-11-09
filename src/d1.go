1b1909065eae62db4b860a96610ba5b34ed2f7e1
David Goldstein <goldstein.g.david@gmail.com>
2024-11-09 16:27:18 -0500

    Day 1 part 3

--- src/d1.go
+++ src/d1.go
@@ -27,24 +27,44 @@ func numPotionsNeededP2(monsters string) int {
 	return potions
 }
 
+func numPotionsNeededP3(monsters string) int {
+	potions := 0
+  for i := 0; i < len(monsters); {
+    potions += potionsNeededFor(rune(monsters[i]))
+    potions += potionsNeededFor(rune(monsters[i+1]))
+    potions += potionsNeededFor(rune(monsters[i+2]))
+
+    
+    monstersInGroup := 0
+    if monsters[i] != 'x' {
+      monstersInGroup += 1
+    }
+
+    if monsters[i+1] != 'x' {
+      monstersInGroup += 1
+    }
+
+    if monsters[i+2] != 'x' {
+      monstersInGroup += 1
+    }
+
+    switch monstersInGroup {
+      case 2: potions += 2
+      case 3: potions += 6
+    }
+
+    i += 3
+  }
+
+	return potions
+}
+
 func potionsNeededFor(mtype rune) int {
   switch mtype {
-    case 'A':
-      {
-        return 0
-      }
-    case 'B':
-      {
-        return 1
-      }
-    case 'C':
-      {
-        return 3
-      }
-    case 'D':
-      {
-        return 5
-      }
-    default: return 0
+    case 'A': return 0
+    case 'B': return 1
+    case 'C': return 3
+    case 'D': return 5
+    default:  return 0
   }
 }
